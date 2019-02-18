5749715a90e31de7460d312019c42a48df4a23ba
pazams <maor@pazams.com>
2019-02-18 14:13:16 -0800

    Pass test

--- index.js
+++ index.js
@@ -15,26 +15,33 @@ module.exports = class DappAuth {
       ethUtil.toBuffer(challenge),
     );
 
-    // Get the address of whoever signed this message
-    const { v, r, s } = ethUtil.fromRpcSig(signature);
-    const recoveredKey = ethUtil.ecrecover(challengeHash, v, r, s);
-    const recoveredAddress = ethUtil.publicToAddress(recoveredKey);
+    let isAuthorizedDirectKey;
 
     // try direct-keyed wallet
-    if (
-      address.toLowerCase() ===
-      ethUtil.bufferToHex(recoveredAddress).toLowerCase()
-    ) {
-      return true;
+    try {
+      // Get the address of whoever signed this message
+      const { v, r, s } = ethUtil.fromRpcSig(signature);
+      const recoveredKey = ethUtil.ecrecover(challengeHash, v, r, s);
+      const recoveredAddress = ethUtil.publicToAddress(recoveredKey);
+
+      if (
+        address.toLowerCase() ===
+        ethUtil.bufferToHex(recoveredAddress).toLowerCase()
+      ) {
+        isAuthorizedDirectKey = true;
+      }
+    } catch (e) {
+      // if either direct-keyed auth flow threw an error, or it did not conclude to be authorized, proceed to try smart-contract wallet in finally clause.
+    } finally {
+      if (isAuthorizedDirectKey === true) return isAuthorizedDirectKey;
+      // try smart-contract wallet
+      const erc1271CoreContract = new this.web3.eth.Contract(ERC1271, address);
+
+      const magicValue = await erc1271CoreContract.methods
+        .isValidSignature(ethUtil.keccak(challenge), signature) // we send just a regular hash, which then the smart contract hashes ontop to an erc191 hash
+        .call();
+
+      return magicValue === ERC1271_MAGIC_VALUE;
     }
-
-    // try smart-contract wallet
-    const erc1271CoreContract = new this.web3.eth.Contract(ERC1271, address);
-
-    const magicValue = await erc1271CoreContract.methods
-      .isValidSignature(ethUtil.keccak(challenge), signature) // we send just a regular hash, which then the smart contract hashes ontop to an erc191 hash
-      .call();
-
-    return magicValue === ERC1271_MAGIC_VALUE;
   }
 };
