ac86e6d6e6e5672254c55f7e9f6b7b06c4ddd56b
pazams <maor@pazams.com>
2019-03-13 11:42:29 -0700

    Cleanup

--- index.js
+++ index.js
@@ -31,17 +31,16 @@ module.exports = class DappAuth {
         isAuthorizedDirectKey = true;
       }
     } catch (e) {
-      // if either direct-keyed auth flow threw an error, or it did not conclude to be authorized, proceed to try smart-contract wallet in finally clause.
-    } finally {
-      if (isAuthorizedDirectKey === true) return isAuthorizedDirectKey;
-      // try smart-contract wallet
-      const erc1271CoreContract = new this.web3.eth.Contract(ERC1271, address);
-
-      const magicValue = await erc1271CoreContract.methods
-        .isValidSignature(ethUtil.keccak(challenge), signature) // we send just a regular hash, which then the smart contract hashes ontop to an erc191 hash
-        .call();
-
-      return magicValue === ERC1271_MAGIC_VALUE;
+      // if either direct-keyed auth flow threw an error, or it did not conclude to be authorized, proceed to try smart-contract wallet.
     }
+    if (isAuthorizedDirectKey === true) return isAuthorizedDirectKey;
+    // try smart-contract wallet
+    const erc1271CoreContract = new this.web3.eth.Contract(ERC1271, address);
+
+    const magicValue = await erc1271CoreContract.methods
+      .isValidSignature(ethUtil.keccak(challenge), signature) // we send just a regular hash, which then the smart contract hashes ontop to an erc191 hash
+      .call();
+
+    return magicValue === ERC1271_MAGIC_VALUE;
   }
 };
