53546c348ae33763bd516f4abbc7ce6d4785f896
pazams <maor@pazams.com>
2019-07-09 17:49:30 -0700

    Improve errors

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ module.exports = class DappAuth {
     );
 
     let isAuthorizedDirectKey;
+    let errEOA;
 
     // try direct-keyed wallet
     try {
@@ -30,17 +31,29 @@ module.exports = class DappAuth {
       ) {
         isAuthorizedDirectKey = true;
       }
-    } catch (e) {
+    } catch (err) {
+      errEOA = err;
       // if either direct-keyed auth flow threw an error, or it did not conclude to be authorized, proceed to try smart-contract wallet.
     }
-    if (isAuthorizedDirectKey === true) return isAuthorizedDirectKey;
-    // try smart-contract wallet
-    const erc1271CoreContract = new this.web3.eth.Contract(ERC1271, address);
+    try {
+      if (isAuthorizedDirectKey === true) return isAuthorizedDirectKey;
+      // try smart-contract wallet
+      const erc1271CoreContract = new this.web3.eth.Contract(ERC1271, address);
 
-    const magicValue = await erc1271CoreContract.methods
-      .isValidSignature(ethUtil.keccak(challenge), signature) // we send just a regular hash, which then the smart contract hashes ontop to an erc191 hash
-      .call();
+      const magicValue = await erc1271CoreContract.methods
+        .isValidSignature(ethUtil.keccak(challenge), signature) // we send just a regular hash, which then the smart contract hashes ontop to an erc191 hash
+        .call();
 
-    return magicValue === ERC1271_MAGIC_VALUE;
+      return magicValue === ERC1271_MAGIC_VALUE;
+    } catch (err) {
+      throw mergeErrors(errEOA, err);
+    }
   }
 };
+
+function mergeErrors(errEOA, errCA) {
+  const msgEOA = errEOA
+    ? `errored with: ${errEOA.toString()}`
+    : 'returned false';
+  return `Authorisation check failed and errored in 2 alternative flows. 'External Owned Account' check ${msgEOA} . 'Contract Account' check errored with: ${errCA.toString()}`;
+}
