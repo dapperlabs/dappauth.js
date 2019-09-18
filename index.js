351bce0b00062e6088e3589087d948a0c2b4c6fa
Nick Salloum <nick@callmenick.com>
2019-09-18 10:37:36 -0700

    adds file extensions to requires

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Web3 = require('web3');
 const ethUtil = require('ethereumjs-util');
-const ERC1271 = require('./ABIs/ERC1271');
+const ERC1271 = require('./ABIs/ERC1271.js');
+const utils = require('./utils.js');
 
 // bytes4(keccak256("isValidSignature(bytes32,bytes)")
 const ERC1271_MAGIC_VALUE = '0x1626ba7e';
